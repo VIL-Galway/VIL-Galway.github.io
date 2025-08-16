8caf688bc1731eac44b9389b95ed7d1ffa2922f1
VIL-Galway <visualintellabgalway@gmail.com>
2025-08-16 03:26:00 +0000

    Deploying to gh-pages from @ VIL-Galway/VIL-Galway.github.io@fbf008a4dbecba036548a719c914f031e8dcd4ef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Materials for courses you taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
